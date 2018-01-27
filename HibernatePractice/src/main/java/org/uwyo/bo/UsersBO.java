931ae5fefc1a5400f25398acbe895643110154d4
Sriharsha <Sriharsha@DESKTOP-3DAQ383.attlocal.net>
2018-01-26 23:36:46 -0800

    Modify

--- HibernatePractice/src/main/java/org/uwyo/bo/UsersBO.java
+++ HibernatePractice/src/main/java/org/uwyo/bo/UsersBO.java
@@ -10,10 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.uwyo.dao.UsersDAO;
 import org.uwyo.model.Users;
-
+//This is Git Modification
 @Service
 public class UsersBO {
-	
+	//Compare
 	@Autowired
 	UsersDAO userDao;
 	
