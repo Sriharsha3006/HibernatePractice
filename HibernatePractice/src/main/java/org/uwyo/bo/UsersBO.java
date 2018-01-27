f28b1c660438a29ebc97f88fac5e460a997be342
Sriharsha Bopparaju <Sriharsha3006@gmail.com>
2018-01-27 01:04:03 -0800

    Removed All Comments

--- HibernatePractice/src/main/java/org/uwyo/bo/UsersBO.java
+++ HibernatePractice/src/main/java/org/uwyo/bo/UsersBO.java
@@ -4,15 +4,15 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import javax.imageio.ImageIO;
-//one change
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.uwyo.dao.UsersDAO;
 import org.uwyo.model.Users;
-//This is Git Modification
+
 @Service
 public class UsersBO {
-	//Compare
+
 	@Autowired
 	UsersDAO userDao;
 	
@@ -21,7 +21,7 @@ public class UsersBO {
 		System.out.println("I am In BO SaveUSer");
 		return userDao.saveUser(user);
 	}
-	//Edited it from Remote Web
+	
 	public void noOfBytesOfImage(){
 		try {
 			File file = new File("D:/hi.jpg");
@@ -29,7 +29,6 @@ public class UsersBO {
 			BufferedImage img = ImageIO.read(file);
 			System.out.println(img);
 		} catch (IOException e) {
-			//hey
 			e.printStackTrace();
 		}
 	}
